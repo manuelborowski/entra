09362583ea307374b3dafb06b0c9d399e55c044e
manuel borowski <emmanuel.borowski@gmail.com>
2023-04-21 09:47:55 +0200

    1.15: small update

--- app/__init__.py
+++ app/__init__.py
@@ -106,10 +106,11 @@ flask_app.config.from_pyfile('config.py')
 # 1.12: papercut, when a badge is re-used, delete the old entry.  Bugfix api, get-staff.  Set default dates in database
 # 1.13: update email of already imported, new staff: resend the invitation if email changed and password not updated yet
 # 1.14: update staff expire date: did not ripple through to AD
+# 1.15: small update
 
 @flask_app.context_processor
 def inject_defaults():
-    return dict(version='@ 2022 MB. V1.13', title=flask_app.config['HTML_TITLE'], site_name=flask_app.config['SITE_NAME'])
+    return dict(version='@ 2022 MB. V1.15', title=flask_app.config['HTML_TITLE'], site_name=flask_app.config['SITE_NAME'])
 
 
 db = SQLAlchemy()
