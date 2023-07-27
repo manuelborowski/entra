49a43f2cd837303b480fde3f1c029398630a3f58
manuel borowski <emmanuel.borowski@gmail.com>
2023-07-27 09:36:27 +0200

    # 1.22: merged from 1.21-informat_student_smartschool-7

--- app/__init__.py
+++ app/__init__.py
@@ -120,11 +120,12 @@ flask_app.config.from_pyfile('config.py')
 # 1.21-informat_student_smartschool-5: update import from informat.  Update export to smartschool
 # 1.21-informat_student_smartschool-6: classroom.cloud specific: use user-login as email
 # 1.21-informat_student_smartschool-7: merged import-staff-from-informat.
+# 1.22: merged from 1.21-informat_student_smartschool-7
 
 
 @flask_app.context_processor
 def inject_defaults():
-    return dict(version='@ 2022 MB. V1.21-informat_student_smartschool-7', title=flask_app.config['HTML_TITLE'], site_name=flask_app.config['SITE_NAME'], testmode = flask_app.testmode)
+    return dict(version='@ 2022 MB. V1.22', title=flask_app.config['HTML_TITLE'], site_name=flask_app.config['SITE_NAME'], testmode = flask_app.testmode)
 
 
 db = SQLAlchemy()
