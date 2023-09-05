f0b7604c275c4a613985b9df0883e6b675291ba3
manuel borowski <emmanuel.borowski@gmail.com>
2023-09-05 15:00:12 +0200

    test

--- app/application/smartschool.py
+++ app/application/smartschool.py
@@ -114,7 +114,9 @@ def __klas_process(db_klassen, teacher_cache):
                 k = k.upper()
                 if k not in teacher_cache:
                     raise Exception(f"{k} is NOT found in Smartschool")
-            __update_titularis(klas.klascode, [teacher_cache[k.upper()] for k in titularissen])
+            titularissen_list = [teacher_cache[k.upper()] for k in titularissen]
+            log.info("TEST titularissen_list", titularissen_list)
+            __update_titularis(klas.klascode, titularissen_list)
 
 
 @exception_wrapper
